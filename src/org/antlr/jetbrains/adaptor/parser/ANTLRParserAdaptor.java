af25780a07aca420c42996d9fe26a76b75b2b24b
Terence Parr <parrt@antlr.org>
2015-12-23 11:13:33 -0800

    oops; remove dead code

--- src/org/antlr/jetbrains/adaptor/parser/ANTLRParserAdaptor.java
+++ src/org/antlr/jetbrains/adaptor/parser/ANTLRParserAdaptor.java
@@ -65,7 +65,6 @@ public abstract class ANTLRParserAdaptor implements PsiParser {
 			ProgressIndicatorProvider.checkCanceled();
 			builder.advanceLexer();
 		}
-		final ParserDefinition parserDefinition = LanguageParserDefinitions.INSTANCE.forLanguage(language);
 		// NOTE: parse tree returned from parse will be the usual ANTLR tree
 		// ANTLRParseTreeToPSIConverter will convert that to the analogous jetbrains AST nodes
 		// When parsing an entire file, the root IElementType will be a
